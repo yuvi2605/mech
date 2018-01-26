b0f5e38d0c0defb127b25c9bba37592518b151d4
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-26 11:47:29 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int a,b;
+    int a,b,r=1;
     printf("enter the number");
-    scanf("%d",&a);
-   b=a*a;
-   printf("power is %d",b);
+    scanf("%d %d",&a,&b);
+    for(int i=0;i<b;i++) {
+   r=r*a;}
+   printf("power is %d",r);
     
 }
+
