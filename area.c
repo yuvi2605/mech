85de6ca8849403728092434a21b51ef7830b87e6
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-26 10:24:16 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int r,a;
+    clrscr();
+    printf("enter the number %d",r);
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("the area of circle is %d",a);
+    getch();
+}
+
