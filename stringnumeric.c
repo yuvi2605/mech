acfe8b577d137a0d8b48f97b1559606aceee625f
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-01 16:49:36 +0530

    Update stringnumeric.c

--- stringnumeric.c
+++ stringnumeric.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-char s[10],i;
+char b[10],i;
 printf("enter a string");
-scanf("%s",&s);
-for(i=0;s[i]!='\0';i++)
+scanf("%s",&b);
+for(i=0;b[i]!='\0';i++)
 {
-if((s[i]>'a')&&(s[i]<'z'))
+if((b[i]>'a')&&(b[i]<'z'))
 {
 printf("no numeric");
 
