c6c631cc537fd621ee9a11d0c85e7ec39ecabc4d
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-04 11:50:11 +0530

    Create 46SET5.C

---
+++ 46SET5.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=n;i<=n;++i)
+    {
+        ++i;
+        printf("%d",i);
+    }
+}
+   
