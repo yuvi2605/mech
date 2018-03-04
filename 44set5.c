4cbf727b810cf35021bfb5dfe88ee7c6cfae769c
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-04 12:03:09 +0530

    Create 44set5.c

---
+++ 44set5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if((a>=1)&&(a<=10))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
