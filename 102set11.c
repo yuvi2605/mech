aace981ee7c88f52a49ee0f20f36a9ee01ef1712
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-13 16:58:47 +0530

    Create 102set11.c

---
+++ 102set11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+   int n,a;
+   printf("enter the number");
+   scanf("%d",&n);
+       if(n%2==0)
+       {
+       a=n/2;
+       printf("%d",a);
+   }
+   else
+   {
+       printf("%d",n);
+   }
+}
+   
