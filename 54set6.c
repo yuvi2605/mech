d4a0cd450742688e00cd72811194203d8e6c0581
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-20 16:30:45 +0530

    Create 54set6.c

---
+++ 54set6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    if(n%2!=0)
+    {
+        printf("%d",n+1);
+    }else
+    {
+    printf("%d",n);
+}}
