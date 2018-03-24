d5e174d6371794546dc9c3473fd93d69461fe820
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-03-24 13:59:39 +0530

    Create IBM1.C

---
+++ IBM1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[10];
+    int i;
+    printf("enter the character");
+    scanf("%s",&a);
+    {
+    for(i=1;a[i]!='\0';i++)
+    {
+        i++;
+    }
+    }
+        printf("%d",i);
+}
